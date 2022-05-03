502f1702a82c7b9b96d2f29d99c1051f45003085
Patrick <maniacxy@protonmail.com>
2022-05-04 00:09:26 +0200

    tried with nav inserter

--- nav-manager.py
+++
@@ -1,9 +0,0 @@
-import os
-from bs4 import BeautifulSoup
-
-with open("templates/navbar.html", "r") as f:
-    navbar = f.read()
-
-files = [file for file in os.listdir(".") if file.endswith(".html")]
-
-print(files[3])
